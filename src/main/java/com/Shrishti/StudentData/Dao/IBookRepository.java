bcad9b36cc5926b9559468ef3b79fc6fc233fba9
Anuhshka Srivastava <shrishtisrivastava19@gmail.com>
2023-02-27 12:37:53 +0530

    Revert "New Commit"
    This reverts commit d9b857d624020854cbb6e07239fc507ac0fef5ac.

--- src/main/java/com/Shrishti/StudentData/Dao/IBookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Shrishti.StudentData.Dao;
-
-import com.Shrishti.StudentData.Model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IBookRepository extends JpaRepository<Book, Integer> {
-}
--- src/main/java/com/Shrishti/StudentData/Dao/ILaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Shrishti.StudentData.Dao;
-
-import com.Shrishti.StudentData.Model.Laptop;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ILaptopRepository extends JpaRepository<Laptop, Integer> {
-}
